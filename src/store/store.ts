30789d1d540866348b823820fc4e8e8d0e642af5
patricnilackshan <nilackshanp@gmail.com>
2024-11-04 01:39:11 +0530

    Add Comment on store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import customerReducer from "./slices/customerSlice";
 
 export const store = configureStore({
+    devTools: false, // Change this to true to enable Redux DevTools on Browser Extension
     reducer:{
         customerState: customerReducer
     }
